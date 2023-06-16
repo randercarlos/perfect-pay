d61cb8a6547ec5bb29ddb2a75538c92205d35373
Rander <randerccf@gmail.com>
2023-06-16 16:16:55 -0300

    adicionada a página de erro

---
+++ resources/views/error.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Falha</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+<h3 style="text-align: center; color: crimson">Falha ao processar o pagamento.</h3>
+<a href="{{ route('checkout') }}" style="display: block; text-align: center">Realizar novo checkout</a>
+</body>
+</html>
